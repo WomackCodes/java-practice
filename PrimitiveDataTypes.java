90bd9d2bafea1dd86c3aa8b2c06cf29ab1577b3d
WomackCodes <chris@womack.codes>
2021-01-04 14:41:24 -0700

    add primitive data types

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,13 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        int var = 1000000000;
+        long biggerNumber = 1000000000000L;
+        short smallNumber = 32767;
+        byte smallerStill = 127;
+        double withDecimal = 500.1d;
+        boolean trueFalse = true;
+        char oneThing = '%';
+        float notNeededUnlessYouWantToSaveMemory = 500f;
+    }
+    
+}
