6db9375b4e4cf8e099145429b003890748350385
WomackCodes <chris@womack.codes>
2021-01-04 14:03:38 -0700

    Variables fun

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int x;
+        x = 4;
+        System.out.println(x);
+
+        x = 23;
+        System.out.println(x + 7);
+        // I can has maths 
+
+        String words;
+        words = "I can write out numbers in a string too like this.  20, 40, 60...";
+        // System.out.println(words);
+        words = "20, 40, 60... or I can change the variable's 'value' to print stuff.  Could always add strings";
+
+        System.out.print(words + " and add words within the print... 80, 100!");
+    }
+    
+}
