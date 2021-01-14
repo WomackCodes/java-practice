674d9ec721d73d486102f3548e9ddc0f6b3162a9
WomackCodes <chris@womack.codes>
2021-01-14 07:40:05 -0700

    add method to test.

--- java_oop/Zoo.java
+++ java_oop/Zoo.java
@@ -20,16 +20,21 @@ public class Zoo {
         // Chicken chick1 = new Chicken("Chicken", 1, "bird feed", 1, false, true);
         // chick1.fly();
 
-        Animal sparrow1 = new Sparrow("sparrow", "seeds", 1, 5, false, true);
-        sparrow1.move();
-        Animal fish1 = new Fish("Fish", 5, "kelp", 8, false, true);
-        fish1.move();
+//         Animal sparrow1 = new Sparrow("sparrow", "seeds", 1, 5, false, true);
+//         sparrow1.move();
+//         Animal fish1 = new Fish("Fish", 5, "kelp", 8, false, true);
+//         fish1.move();
 
-        moveAnimal(fish1);
-        moveAnimal(sparrow1);
-    }
+//         moveAnimal(fish1);
+//         moveAnimal(sparrow1);
+//     }
 
-        public static void moveAnimal(Animal animal) {
-            animal.move();
-    }
+//         public static void moveAnimal(Animal animal) {
+//             animal.move();
+//     }
+
+    CanFly flyingBird = new Sparrow("sparrow", "birdseed", 1, 3, false, true);
+    flyingBird.fly();
+}
+
 }
