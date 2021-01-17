f6099cdde62a06cdfe134cf8fe3e9999f5bacdab
WomackCodes <chris@womack.codes>
2021-01-16 21:54:12 -0700

    practice with strings

---
+++ more_practice/StringsPractice.java
@@ -0,0 +1,28 @@
+package more_practice;
+
+public class StringsPractice {
+    		
+	public static void main(String[] args) {
+		
+//		
+//		String str = "ABCD";
+//		
+//		String extractedString = str.substring(1,3);
+//		
+//		System.out.println(extractedString);
+		
+		String a = "hello1";
+		String b = "goodbye1";
+		
+		if (!a.equals("hello")){
+			System.out.println("Something is wrong with A");
+		}
+		
+		if (!b.equals("goodbye")) {
+			System.out.println("Something is wrong with B");
+
+		}
+
+	}
+}
+
