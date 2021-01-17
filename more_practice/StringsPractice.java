f1c20c92ad4dbbb2e6481e7d8c048a25dc10f2c5
WomackCodes <chris@womack.codes>
2021-01-17 14:38:32 -0700

    fun with bang operator

--- more_practice/StringsPractice.java
+++ more_practice/StringsPractice.java
@@ -11,16 +11,19 @@ public class StringsPractice {
 //		
 //		System.out.println(extractedString);
 		
-		String a = "hello1";
-		String b = "goodbye1";
+		String a = "hello";
+		String b = "goodbye";
 		
 		if (!a.equals("hello")){
 			System.out.println("Something is wrong with A");
+		} else {
+			System.out.println("Yep, a equals 'hello'.");
 		}
 		
 		if (!b.equals("goodbye")) {
 			System.out.println("Something is wrong with B");
-
+		} else {
+			System.out.println("Yep, b equals 'goodbye'.");
 		}
 
 	}
